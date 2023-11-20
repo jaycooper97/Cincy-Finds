194e706199577420679f2715d45c5a2ac011eb9f
jay cooper <15jacooper@gmail.com>
2023-11-20 18:14:39 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+//React Router v6 App
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route, /*Link, useRouteMatch*/ } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+import Contact from './Contact';
+import Food from './Food';
+import Home from './Home';
 import './App.css';
+import './Home.css';
+import Navbar from './Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+
+      <BrowserRouter>
+        <div>
+          <Routes>
+            {<Home />}
+            {<Navbar />}
+            <br />
+
+            {/* <Footer /> */}
+
+            {/*<Route exact path='/' element={<Home />} />*/}
+            <Route exact path='Contact/' element={<Contact />} />
+            <Route path='Food/' element={<Food />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/Contact.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import City from './img/City.avif';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="title">We'd love to hear from you!</h3>
+        <br />
+        <br />
+        <img src={City} alt="City" className="comp-img" />
+        <br />
+        <br />
+        <p>Here at Cincy Finds, we strive to listen to our customers' voices and feedback; it's imperative that we understand your needs, and if you have any questions or concerns, you can complete the form below;we will respond as quickly as possible.</p>
+        <br />
+        <br />
+        <h3 className="title">Tell us everything...</h3>
+        <div className="container">
+          <form action="">
+            <input className="input" type="text" placeholder="First name" id="first-name" />
+
+            <input className="input" type="text" placeholder='Last name' id="last-name" />
+
+            <input className="input" type="text" placeholder='Email' id="email" />
+
+            <textarea className="input" name="" cols="30" rows="10" placeholder='How can we help you?' id="help"></textarea>
+
+            <input className='input' style="width: 100px; height: 60px;" type="submit" placeholder='Submit' />
+          </form>
+        </div>
+
+        <h3 className='title'>Call or email us today!</h3>
+        <br />
+        <h3 className='title'>Phone number: (513) 628-4422 <br /> Email: cincy_finds_admin2023@yahoo.com</h3>
+        <br />
+        <br />
+      <h3 className="title">Come and see us anytime!</h3>
+      <br />
+      <p>Hours: Monday-Friday: 10a-10p <br /> Saturday: 12p-12a <br /> Sunday: 12p-10p</p>
+      <br />
+      <br />
+      <p>Thanks for stopping by Cincy Finds - Anything for our people!</p>
+      </div>
+    )
+  }
+}
+
+
+export default Contact
